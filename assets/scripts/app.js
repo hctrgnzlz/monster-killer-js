390de8f2bf731a664d73b9c24e561b2bec444424
hctrgnzlz <hectorgzz89@gmail.com>
2023-11-14 15:42:30 -0500

    healing functionality w/ if check

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,27 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
-const MONSTER_ATTACK_VALUE = 14;
-
+const MONSTER_ATTACK_VALUE = 21;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+function endRound() {
+  const playerDamage = dealPlayerDamage (MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
+  }
+}
+
 
 function attackMonster (attackMode) {
   let maxDamage;
@@ -18,27 +32,32 @@ function attackMonster (attackMode) {
   }
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage; //currentMonsterHealth = currentMosterHealth - damage
-  
-  const playerDamage = dealPlayerDamage (MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage
-
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert('You won!');
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert('You lost!');
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0)
-    alert('You have a draw!');
+  endRound();
 }
 
 
-function attackHandler(){
+function attackHandler() {
   attackMonster('ATTACK')
 }
 
-function strongAttackHandler(){
+function strongAttackHandler() {
   attackMonster('STRONG_ATTACK')
 }
 
+function healPlayerHandler () {
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert ("You can't heal to more than your max health");
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+
+  increasePlayerHealth(HEAL_VALUE);
+  currentPlayerHealth += healValue;
+  endRound();
+}
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
