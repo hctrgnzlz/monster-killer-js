09eeab23e81aadd9c820087675188032db0326ef
hctrgnzlz <hectorgzz89@gmail.com>
2024-01-05 14:33:58 -0500

    loops and error handling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,15 +11,33 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enteredValue = prompt('Maximum life for you and the monster.', '100');
 
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let lastLoggedEntry;
 
-//is not a number or negative number
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
-  chosenMaxLife = 100;
+function getMaxLifeValues () {
+  const enteredValue = prompt('Maximum life for you and the monster.', '100');
+  const parsedValue = parseInt(enteredValue);
+
+  //is not a number or negative number
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw {message: 'invalid user input!'};
+  }
+  return parsedValue;
 }
+
+let chosenMaxLife;
+
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
+  chosenMaxLife = 100;
+  alert('You entered something wrong, default value of 100 was used.');
+}
+
+
+
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -199,7 +217,40 @@ function healPlayerHandler () {
 }
 
 function printLogHandler() {
-  console.log(battleLog);
+  for (let i = 0; i < 3; i++) {
+    console.log('-------')
+  }
+  let j = 0;
+  outerWhile: do {
+    console.log('Outer', j);
+    innerFor: for (let k = 0; k < 5; k++) {
+      if (k === 3) {
+        break outerWhile;
+      }
+      console.log('Inner', k);
+    }
+    j++;
+  } while (j < 3);
+  // for (let i = 10; i > 0; i--) {
+  //   console.log(i);
+  // }
+
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log(battleLog[i])
+  // }
+  let i = 0;
+  for (const logEntry of battleLog) {
+    if (!lastLoggedEntry && lastLoggedEntry !== 0|| lastLoggedEntry < i) {
+      console.log(`#${i}`);
+      for (const key in logEntry) {
+        console.log(`${key} => ${logEntry[key]}`);
+      }
+      lastLoggedEntry = i;
+      break;
+    }
+    i++
+   
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
